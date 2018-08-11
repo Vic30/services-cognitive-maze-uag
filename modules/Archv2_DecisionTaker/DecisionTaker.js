3fa5117ba482a0dc0c56d8ebec7bf23b37d3065c
Unknown <jpacov@gmail.com>
2018-08-10 20:30:05 -0500

    New files for redesign of architecture

---
+++ modules/Archv2_DecisionTaker/DecisionTaker.js
@@ -0,0 +1,15 @@
+const curiosity = require('.././cognitiveRegulations/Curiosity');
+const objetive = require('.././cognitiveRegulations/Objetive');
+const reasoning = require('.././cognitiveRegulations/Reasoning');
+
+
+
+function chooseAPath() {
+    checkPossiblePaths();
+
+}
+
+
+module.exports = {
+    chooseAPath,
+};
